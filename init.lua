a4f12099b39c828472db9733077e28dcd67d3bbd
Serious-Fin <augustasgri@gmail.com>
2024-10-04 08:14:32 +0300

    add basic config

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap('n', '<leader>e', ':e ~/.config/nvim/init.lua<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wq', ':wq<CR>', { noremap = true, silent = true })
