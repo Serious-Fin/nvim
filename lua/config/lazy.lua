4846074ba7cfe29c8b374172e869db4c9931b48d
Serious-Fin <augustasgri@gmail.com>
2024-10-05 00:10:48 +0300

    add lualine and modularize implementation

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,21 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.api.nvim_set_keymap('n', '<leader>e', ':e ~/.config/nvim/init.lua<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>wq', ':wq<CR>', { noremap = true, silent = true })
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.wrap = false
-vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
-
+require("vim-options")
 
 -- Setup lazy.nvim
 require("lazy").setup({
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,14 @@
 return {
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = { "bash", "c", "cpp", "css", "csv", "dockerfile", "elixir", "gitignore", "gitcommit", "go", "gomod", "gosum", "html", "javascript", "java", "json", "lua", "php", "python", "rust", "sql", "svelte", "typescript", "vim", "zig", "vimdoc", "query", "heex"},
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
+        })
+    end
 }
+
