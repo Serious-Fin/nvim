e63f825ac50a9534e003b849859a2ca880473c44
Serious-Fin <augustasgri@gmail.com>
2024-10-04 22:45:15 +0300

    added treesitter, telescope and catppuccin theme

--- init.lua
+++ init.lua
@@ -1,14 +1,20 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.wrap = false
-vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
+require("config.lazy")
 
-vim.g.mapleader = " "
-vim.api.nvim_set_keymap('n', '<leader>e', ':e ~/.config/nvim/init.lua<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>wq', ':wq<CR>', { noremap = true, silent = true })
+--  Setup Catppuccin Theme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin-mocha"
+
+-- Setup Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Telescope live grep' })
+
+-- Setup Treesitter
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+    ensure_installed = { "bash", "c", "cpp", "css", "csv", "dockerfile", "elixir", "gitignore", "gitcommit", "go", "gomod", "gosum", "html", "javascript", "java", "json", "lua", "php", "python", "rust", "sql", "svelte", "typescript", "vim", "zig", "vimdoc", "query", "heex"},
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },  
+})
---
+++ lua/config/lazy.lua
@@ -0,0 +1,40 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.api.nvim_set_keymap('n', '<leader>e', ':e ~/.config/nvim/init.lua<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wq', ':wq<CR>', { noremap = true, silent = true })
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
