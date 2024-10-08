a4d5dfd24dec33ad1bad7859af4988ce8b7d9f07
Serious-Fin <augustasgri@gmail.com>
2024-10-08 23:03:26 +0300

    added lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls" }
+                ensure_installed = { "lua_ls", "ts_ls", "jsonls", "gopls", "html", "bashls", "clangd", "cssls", "dockerls", "elixirls", "phpactor", "vimls", "sqlls", "svelte", "rust_analyzer", "zls" }
             })
         end
     },
@@ -19,6 +19,21 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
+            lspconfig.jsonls.setup({})
+            lspconfig.gopls.setup({})
+            lspconfig.html.setup({})
+            lspconfig.bashls.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.cssls.setup({})
+            lspconfig.dockerls.setup({})
+            lspconfig.elixirls.setup({})
+            lspconfig.phpactor.setup({})
+            lspconfig.pylsp.setup({})
+            lspconfig.vimls.setup({})
+            lspconfig.sqlls.setup({})
+            lspconfig.svelte.setup({})
+            lspconfig.rust_analyzer.setup({})
+            lspconfig.zls.setup({})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
         local configs = require("nvim-treesitter.configs")
@@ -7,7 +7,7 @@ return {
             ensure_installed = { "bash", "c", "cpp", "css", "csv", "dockerfile", "elixir", "gitignore", "gitcommit", "go", "gomod", "gosum", "html", "javascript", "java", "json", "lua", "php", "python", "rust", "sql", "svelte", "typescript", "vim", "zig", "vimdoc", "query", "heex"},
             sync_install = false,
             highlight = { enable = true },
-            indent = { enable = true },  
+            indent = { enable = true },
         })
     end
 }
