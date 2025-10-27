48347e13162c84066cb629d883c8b64af5963c4a
Serious-Fin <augustasgri@gmail.com>
2025-10-27 22:36:46 +0200

    enable in-line error messages

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,7 @@ options.tabstop = 4 -- Set <tab> width to 4
 options.shiftwidth = 4 -- Set auto-indentation to 4
 options.expandtab = false -- Do not expoand tabs to spaces
 -- TODO: think about wrap and textwidth
+
+vim.diagnostic.config({
+  virtual_text = true
+})
