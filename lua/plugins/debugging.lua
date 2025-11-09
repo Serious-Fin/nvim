d37f05ee2f47f6c15deab6bff420738b9b3f8ed0
Serious-Fin <augustasgri@gmail.com>
2025-11-09 17:54:52 +0200

    add rust debugger

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,78 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = { "rcarriga/nvim-dap-ui" }, 
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			require("dapui").setup()
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+					args = { "--port", "${port}" },
+				},
+			}
+
+			dap.configurations.rust = {
+				{
+					name = "Launch (LLDB)",
+					type = "codelldb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					args = {},
+					cwd = "${workspaceFolder}",
+					stopOnEntry = true,
+				},
+				{
+					name = "Select and attach to process (LLDB)",
+					type = "codelldb",
+					request = "attach",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					pid = function()
+						local name = vim.fn.input("Executable name (filter): ")
+						return require("dap.utils").pick_process({ filter = name })
+					end,
+					cwd = "${workspaceFolder}",
+				},
+			}
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<F5>", function()
+				dap.continue()
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				dap.step_over()
+			end)
+			vim.keymap.set("n", "<F11>", function()
+				dap.step_into()
+			end)
+			vim.keymap.set("n", "<F12>", function()
+				dap.step_out()
+			end)
+			vim.keymap.set("n", "<Leader>b", function()
+				dap.toggle_breakpoint()
+			end)
+		end,
+	},
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+}
